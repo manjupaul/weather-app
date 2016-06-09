07707254de763fee95cac6e12935cab2f5dd58f0
Manju <manjupaul1@gmail.com>
2016-06-09 15:09:05 -0400

    #3 added date format

--- controller.js
+++ controller.js
@@ -14,7 +14,7 @@ weatherApp.controller('forecastController',['$scope', '$resource', 'cityService'
 
     $scope.weatherAPI =
         $resource("http://api.openweathermap.org/data/2.5/forecast/daily", {callback :"JSON_CALLBACK"},{ get:{method:"JSONP"}});
-    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2, APPID: 'xxxxxxxxxxxxxxxx'});
+    $scope.weatherResult = $scope.weatherAPI.get({q: $scope.city, cnt: 2, APPID: 'd1865783d21b6e848db41a8a1237d7bb'});
 
     //covert to farenheit
     $scope.convertToFarenheit = function(degK){
